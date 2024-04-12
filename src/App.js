120541733072e1d50024161fddd1ffc2a140ade4
Amazingspy <amazingspidey348@gmail.com>
2024-04-12 23:21:10 +0530

    Portfolio project initiated and commited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
+import Resume from './Components/Resume/Resume'
+import Contact from "./Components/Contact/Contact"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      {/* <h1>Welcome to my website!</h1>
+      <p>This is a simple portfolio website built using React.</p> */}
+      <main className='main'>
+        <Home />
+        <About />
+        <Projects />
+        <Resume />
+        <Contact />
+      </main>
+
+
     </div>
   );
 }

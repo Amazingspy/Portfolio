be855037a0f95b6a84793ff39da37b63de2c7cda
Amazingspy <amazingspidey348@gmail.com>
2024-04-12 23:43:10 +0530

    routes implemented

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css'
 import Home from './Components/Home/Home'
 import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
 import Resume from './Components/Resume/Resume'
 import Contact from "./Components/Contact/Contact"
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <h1>Welcome to my website!</h1>
-      <p>This is a simple portfolio website built using React.</p> */}
       <main className='main'>
-        <Home />
-        <About />
-        <Projects />
-        <Resume />
-        <Contact />
+        
+
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+
+        </Router>
+
       </main>
 
 
